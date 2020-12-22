f9a1f4996f734722ef0eb5bf8bed7d11263ab27a
XxxxHDxxxX <72903162+XxxxHDxxxX@users.noreply.github.com>
2020-12-22 10:16:00 +0800

    Create bikacg.js

---
+++ bikacg.js
@@ -0,0 +1,54 @@
+const cookieName = '哔咔'
+const Location Key = 'chavy_ Location_bikacg'
+const chavy = init()
+const LocationVal = $request.headers['Location']
+if (cookieVal) {
+  if (chavy.setdata(cookieVal, Location Key)) {
+    chavy.msg(`${cookieName}`, '获取哔咔Location: 成功', '')
+    chavy.log(`[${cookieName}] 获取哔咔Location: 成功, Location: ${LocationVal}`)
+  }
+}
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  done = (value = {}) => {
+    $done(value)
+  }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+}
+chavy.done()
