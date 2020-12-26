03c02e2f9a53193e7d988adbf03f010d95b3ea33
XxxxHDxxxX <72903162+XxxxHDxxxX@users.noreply.github.com>
2020-12-26 12:45:19 +0800

    Update jd_fruit.js

--- jd_fruit.js
+++ jd_fruit.js
@@ -29,9 +29,9 @@ let cookiesArr = [], cookie = '', jdFruitShareArr = [], isBox = false, notify, n
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // 这个列表填入你要助力的好友的shareCode
    //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
-  '540631ec922c4138a8a2d3ed2a0072a5@a39b9e8cac3948dd8f89064c66022190@36c153c277ef4d1a880617dd4f228354@403692f8944b44df8f35300bcfaadb6b@7d9d02b90b1a404db5002444076de16a@ d39561e08faf4dc58f38295eff263863',
+  '540631ec922c4138a8a2d3ed2a0072a5@6c58da357e9443f1a390ec8a0db8df26@a39b9e8cac3948dd8f89064c66022190@36c153c277ef4d1a880617dd4f228354@403692f8944b44df8f35300bcfaadb6b@7d9d02b90b1a404db5002444076de16a@ d39561e08faf4dc58f38295eff263863',
   //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
-  '540631ec922c4138a8a2d3ed2a0072a5@a39b9e8cac3948dd8f89064c66022190@36c153c277ef4d1a880617dd4f228354@403692f8944b44df8f35300bcfaadb6b@7d9d02b90b1a404db5002444076de16a@ d39561e08faf4dc58f38295eff263863',
+  '540631ec922c4138a8a2d3ed2a0072a5@6c58da357e9443f1a390ec8a0db8df26@a39b9e8cac3948dd8f89064c66022190@36c153c277ef4d1a880617dd4f228354@403692f8944b44df8f35300bcfaadb6b@7d9d02b90b1a404db5002444076de16a@ d39561e08faf4dc58f38295eff263863',
 ]
 let message = '', subTitle = '', option = {}, isFruitFinished = false;
 const retainWater = 100;//保留水滴大于多少g,默认100g;
